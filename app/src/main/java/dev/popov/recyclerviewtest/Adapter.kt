2b34536296057c03e757cad00a2b7e1342efe5a1
Sergey Popov <popov.workmail@gmail.com>
2022-02-11 17:15:42 +0500

    Commit

--- app/src/main/java/dev/popov/recyclerviewtest/Adapter.kt
+++ app/src/main/java/dev/popov/recyclerviewtest/Adapter.kt
@@ -1,15 +1,41 @@
 package dev.popov.recyclerviewtest
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import dev.popov.recyclerviewtest.databinding.RvItemBinding
 
-class Adapter: RecyclerView.Adapter<Adapter.ViewHolder> {
+class Adapter: RecyclerView.Adapter<Adapter.ViewHolder>() {
+    private var data = ArrayList<Model>()
     class ViewHolder(item: View): RecyclerView.ViewHolder(item) {
         private val binding = RvItemBinding.bind(item)
         fun bind(data: Model) = with(binding){
             imageGen.setImageResource(data.imageID)
             textTitle.text = data.title
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.rv_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+    fun addViewItem(item: DataForRV){
+        for (i in 1..item.imageID.size){
+            for (j in 1..item.textItem.size){
+                val dataItem = Model(item.imageID[i], item.textItem[j])
+                data.add(dataItem)
+            }
+        }
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/dev/popov/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/dev/popov/recyclerviewtest/MainActivity.kt
@@ -2,10 +2,23 @@ package dev.popov.recyclerviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import dev.popov.recyclerviewtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = Adapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+    private fun init() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(this@MainActivity)
+        rcView.adapter = adapter
+        adapter.addViewItem(item = DataForRV)
+    }
+
 }
